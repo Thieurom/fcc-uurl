4f2e2089aedee2462addf37be4f04f0712e6b7c9
Doan Le Thieu <thieurom@gmail.com>
2016-11-29 18:16:14 +0700

    Setup deps and basic config app

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const express = require('express');
+const mongo = require('mongodb');
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+// Serve static files
+app.use('/static', express.static('public'));
+
+// Home page route
+app.get('/', (req, res) => {
+    res.send('It works!');
+});
+
+// Listen on incoming requests
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT} ...`);
+});
