dbfcde02d6a70db38a0548ed5e8c032737393a8f
Doan Le Thieu <thieurom@gmail.com>
2016-11-30 20:18:14 +0700

    Add shorten route

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 'use strict'
 
 const express = require('express');
-const mongo = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
+const validUrl = require('valid-url');
+const hash = require('string-hash');
 
 const PORT = process.env.PORT || 8080;
+const DATABASE = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/data-dev';
+
 const app = express();
 
 // Serve static files
@@ -11,7 +15,47 @@ app.use('/static', express.static('public'));
 
 // Home page route
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: './public'});
+    res.sendFile('index.html', { root: './public' });
+});
+
+// Shorten route
+app.get('/shorten/:url(*)', (req, res) => {
+    let url = req.params.url;
+
+    if (validUrl.isWebUri(url)) {
+        // Query database
+        MongoClient.connect(DATABASE, (err, db) => {
+            if (err) throw err;
+            let collection = db.collection('shortUrls');
+
+            collection.findOneAndUpdate({url: url}
+                , {
+                    $setOnInsert:
+                    {
+                        url: url,
+                        shortUrl: req.protocol + '://' + req.hostname + '/' + hash(url)
+                    }
+                }
+                , {
+                    returnOriginal: false,
+                    upsert: true
+                }
+                , (err, doc) => {
+                    if (err) throw err;
+                    console.log(doc);
+
+                    res.json({
+                        original_url: doc.value.url,
+                        short_url: doc.value.shortUrl
+                    });
+
+                    db.close();
+            });
+        });
+
+    } else {
+        res.json({ error: 'Wrong url format, make sure you have a valid protocol and real site.' });
+    }
 });
 
 // Listen on incoming requests
